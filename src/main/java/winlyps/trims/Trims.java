65d1bae8d4eedb480e96552a7a2a953efbea47cd
Winlypse <nts3@o2.pl>
2024-07-12 18:49:10 +0200

    Inital commit

---
+++ src/main/java/winlyps/trims/Trims.java
@@ -0,0 +1,81 @@
+package winlyps.trims;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Trims extends JavaPlugin implements CommandExecutor {
+
+    private static final Map<String, Material> MATERIAL_MAP = new HashMap<>();
+    private static final Map<String, String> PATTERN_MAP = new HashMap<>();
+
+    static {
+        MATERIAL_MAP.put("quartz", Material.QUARTZ);
+        MATERIAL_MAP.put("iron", Material.IRON_INGOT);
+        MATERIAL_MAP.put("gold", Material.GOLD_INGOT);
+        MATERIAL_MAP.put("diamond", Material.DIAMOND);
+        MATERIAL_MAP.put("netherite", Material.NETHERITE_INGOT);
+
+        PATTERN_MAP.put("vex", "minecraft:vex");
+        PATTERN_MAP.put("tide", "minecraft:tide");
+        PATTERN_MAP.put("wild", "minecraft:wild");
+        PATTERN_MAP.put("ward", "minecraft:ward");
+        PATTERN_MAP.put("eye", "minecraft:eye");
+    }
+
+    @Override
+    public void onEnable() {
+        // Register the command executor
+        getCommand("trims").setExecutor(this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be used by players.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        if (args.length != 1) {
+            player.sendMessage("Usage: /trims <material>");
+            return true;
+        }
+
+        String materialName = args[0].toLowerCase();
+        Material material = MATERIAL_MAP.get(materialName);
+        if (material == null) {
+            player.sendMessage("Invalid material.");
+            return true;
+        }
+
+        ItemStack itemInHand = player.getInventory().getItemInMainHand();
+        if (itemInHand.getType().isAir()) {
+            player.sendMessage("You must hold an item in your hand.");
+            return true;
+        }
+
+        String itemType = itemInHand.getType().getKey().toString();
+        for (Map.Entry<String, String> entry : PATTERN_MAP.entrySet()) {
+            String pattern = entry.getValue();
+            String commandString = String.format("give %s %s{Trim:{material:\"minecraft:%s\", pattern:\"%s\"}} 1",
+                    player.getName(), itemType, materialName, pattern);
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commandString);
+        }
+
+        return true;
+    }
+}
